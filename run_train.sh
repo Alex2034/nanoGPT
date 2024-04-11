3ca939e38e39523ae31c1b01d9b62b245151c0c1
root <fokin.aa@phystech.edu>
2024-04-11 16:05:17 +0000

    pics

--- run_train.sh
+++ run_train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Running train_script.py in hyperbolic mode"
-python train_script.py --epochs=800 --mode=hyperbolic --batch_size=200 --block_size=128 --gpu=
+python train_script.py --epochs=800 --mode=hyperbolic --batch_size=200 --block_size=128 
 
 echo "Running train_script.py in original mode"
 python train_script.py --epochs=800 --mode=original --batch_size=1000 --block_size=128
